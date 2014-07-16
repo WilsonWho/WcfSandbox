c0fa9015af94decdc1f70215c69e1a587ac3d5b5
wilsohn <Wilson.Tzou@iqmetrix.com>
2014-07-16 14:19:09 -0700

    Moving WCF.Contracts to WCF.Common folder

---
+++ src/WCF/WCF.Common/ICommunicationChannel.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel;
+
+namespace WCF.Common
+{
+    [ServiceContract(Name = "CommunicationChannel")]
+    public interface ICommunicationChannel
+    {
+        [OperationContract]
+        [FaultContract(typeof(CalculatorFault))]
+        void DonateBag(BagOfGoodies bagOfGoodies);
+
+        [OperationContract]
+        [FaultContract(typeof(CalculatorFault))]
+        decimal Divide(decimal val1, decimal val2);
+
+        [OperationContract]
+        [FaultContract(typeof(CalculatorFault))]
+        decimal SafeDivide(decimal numerator, decimal denominator);
+    }
+}
